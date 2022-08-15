1a6217f6d77bda32f07296b6447a9ed36064ccce
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-08-15 14:12:59 -0400

    Feat: Adiciona a camada models do products e configura o arquivo .env

--- models/products.js
+++ models/products.js
@@ -1,1 +1,21 @@
-// Iniciando o Projeto
+const connection = require('./connection');
+
+const productsModel = {
+  productsAll: async () => {
+    const [products] = await connection.execute(
+      'SELECT * FROM StoreManager.products;',
+    );
+    return products;
+  },
+
+  productsId: async (id) => {
+    const [product] = await connection.execute(
+      'SELECT * FROM StoreManager.products WHERE id = ?;',
+      [id],
+    );
+    return product;
+  },
+
+};
+
+module.exports = productsModel;
